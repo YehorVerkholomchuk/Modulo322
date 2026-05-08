8a2ce279b14f92cbad90a898499f4ae10008ae20
fabiomaruca-sudo <fabio10maruca@gmail.com>
2026-05-08 13:22:38 +0200

    Inizio progetto

---
+++ 2_Applicativo/MyLibrary - Fabio/MyLibrary/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MyLibrary
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
