d85398f2faf35f7e5cedcb056f81c5615962243e
YehorVerkholomchuk <yehor.verkholomchuk@samtrevano.ch>
2026-05-22 15:01:54 +0200

    update

--- 2_Applicativo/MyLibrary - Fabio/MyLibrary/obj/Debug/net8.0-android/MyLibrary.AssemblyInfo.cs
+++ 2_Applicativo/MyLibrary - Fabio/MyLibrary/obj/Debug/net8.0-android/MyLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+63fc0d96971785ccd8ab6c296a4d73f3c207d335")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+1f2dd719ed835bb879521175e86e08ae4f0278c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
